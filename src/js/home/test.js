8adbe02d1b712a087456fa6bfbed452661da92b0
enjing <578951992@qq.com>
2016-12-06 10:09:59 +0800

    change project

---
+++ src/js/home/test.js
@@ -0,0 +1,9 @@
+import myHead from '../../components/home/home-header.vue';
+
+var V = new Vue({
+	el: '#container',
+	components: {
+		myHead
+	}
+});
+  
---
+++ src/js/shopping/buy.js
@@ -0,0 +1,10 @@
+import List from '../../components/shopping/buy-list.vue';
+import Loading from '../../components/common/loading.vue';
+
+var V = new Vue({
+	el: '#container',
+	components: {
+		List,Loading
+	}
+});
+      
